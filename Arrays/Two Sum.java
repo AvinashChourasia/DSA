55021e10fc79a684722c770ddde7da6839469703
Avinash Chourasia <97829396+AvinashChourasia@users.noreply.github.com>
2022-10-18 19:14:49 +0530

    Create Two Sum.java

---
+++ Arrays/Two Sum.java
@@ -0,0 +1,19 @@
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int a[] = new int[2];
+        HashMap<Integer,Integer> h = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(h.containsKey(target - nums[i])){
+                a[0] = h.get(target-nums[i]);
+                a[1] = i;
+            }
+            h.put(nums[i],i);
+        }
+
+        return a;
+    }
+}
